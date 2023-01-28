0837ecc735ecdef0dda5e811e0632d5ec1b98141
Eugenii.Sivkov <EugeniiSivkov@LiveNation.com>
2023-01-28 16:17:35 -0300

    правка старых заданий + 1 новое

--- 1 lesson/weight.rb
+++ 1 lesson/weight.rb
@@ -4,7 +4,8 @@ name = gets.chomp
 puts "Какой ваш рост?"
 height = gets.chomp
 
-if height.to_i - 110 < 0
+best_height = (height.to_i - 110) * 1.15
+if best_height < 0
   puts "Ваш вес уже оптимальный"
-else puts "#{name}, ваш идеальный вес #{height.to_i - 110}"
+else puts "#{name}, ваш идеальный вес #{best_height}"
 end
