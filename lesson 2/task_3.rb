9c8c7fec6be20e2bbd14510af1e6b748f1da15d8
Eugenii.Sivkov <EugeniiSivkov@LiveNation.com>
2023-01-31 18:27:25 -0300

    числа Фиббоначи

---
+++ lesson 2/task_3.rb
@@ -0,0 +1,13 @@
+#Заполнить массив числами фибоначчи до 100 
+array = []
+fib1 = 0
+fib2 = 1
+fib3 = 0
+while fib3 < 100 do
+    array << fib3
+    fib3 = fib1 + fib2
+    fib1 = fib2
+    fib2 = fib3
+end
+
+#puts array
