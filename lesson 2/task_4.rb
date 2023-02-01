08d9165d5ca7c9c3ddd8d1ed48cd0287b633b8b9
Eugenii.Sivkov <EugeniiSivkov@LiveNation.com>
2023-02-01 18:55:50 -0300

    fix previous commit

---
+++ lesson 2/task_4.rb
@@ -0,0 +1,9 @@
+# Заполнить хеш гласными буквами, где значением будет являтся порядковый номер буквы в алфавите (a - 1).
+array = ["a", "e", "i", "o", "u", "y"]
+shift = 96
+hash = {}
+for i in array do
+    hash.store(i, i.ord - shift)
+end
+
+#puts hash
